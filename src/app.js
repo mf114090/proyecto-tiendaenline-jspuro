cba23a24e6b697d433243bd992c48518a257f88e
Manuel Filgueira Marin <manuelfilgueira@gmail.com>
2022-02-11 02:20:54 +0000

    proyecto avanzado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/img1.jpg";
+import "./assets/img/img2.jpg";
+import "./assets/img/img3.jpg";
+import "./assets/img/img4.jpg";
+import "./assets/img/img5.jpg";
+import "./assets/img/img6.jpg";
+import "./assets/img/img7.jpg";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// funcion para traer datos de base Sql
+
+const inputBsucar = document.getElementById("buscar");
+const celdas = document.getElementById("td");
+
+let uri = "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com";
+fetch(uri)
+  .then(response => response.json())
+  .then(json => MostrarDatos(json))
+  .catch(e => console.log(e));
+
+const mostrarData = data => {};
+let body = "";
+for (let i = 0; i < data.length; i++) {
+  body += ´
+    <tr>
+      <td>${data[i].id}</td>
+      <td>${data[i].name}</td>
+      <td>${data[i].url_image}</td>
+      <td>${data[i].discount}</td>
+      <td>${data[i].category}</td>
+    </tr>
+  ´}
+  document.querySelector('.datos').innerHTML=body
+
+  inputBsucar.addEventListener('keyup', (e)=> {e.target})
+
